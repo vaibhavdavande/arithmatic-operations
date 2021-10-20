278686a999b6a478c3dcebca31bac571a49131b3
vaibhavdavande <vaibhav.davande@gmail.com>
2021-10-20 15:35:17 +0530

    Add files via upload

---
+++ firstprog.py
@@ -0,0 +1,24 @@
+a=20
+b=30
+c=a+b
+print('Addition is', c)
+d=b-a
+print('Subtraction is',d)
+e=b*c
+print('Multiplication is',e)
+f=b/c
+print('Division is', f)
+h=a**2
+print('Square of a is', h)
+h=a//2
+print('Flour division', h)
+
+if a>b:
+    print ('a is smaller than b')
+else :
+    print ('a is greater than b')
+
+
+
+
+        
